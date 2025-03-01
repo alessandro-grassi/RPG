ed87eb1016b3579454837c8fc51847d8f7b14d40
alessandro-grassi <grassi.alessandro06@gmail.com>
2025-03-01 10:32:50 +0100

    first commit

---
+++ Back-end/moduli/modulo/script.py
@@ -0,0 +1,15 @@
+def check_get(path):
+    if path.endswith("get_generica"):
+        return funz()
+    
+def check_post(path,client_choice):
+    if path.endswith("post_generica"):
+        return f2(client_choice)
+    
+def funz():
+    print("ciao")
+
+def f2(client_choice):
+    nome = client_choice.get("nome","")
+    cognome = client_choice.get("cognome","")
+    print("ciao" + nome)
