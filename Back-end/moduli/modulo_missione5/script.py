e069049065a8fbc1d5d489f32d82061ca863063e
flekss07 <edopia007@gmail.com>
2025-03-14 14:33:48 +0100

    cambiato codice server

--- Back-end/moduli/modulo_missione5/script.py
+++ Back-end/moduli/modulo_missione5/script.py
@@ -3,8 +3,19 @@ from moduli import queryLib
 #queryLib.connetti()
 
 def check_get(path):
-    return path
-
+    if path == "/m5/styletest": # stest get fogli stile
+        with open("../Missioni/Missione5/html_pages/style-template.css", "r") as f:
+            r = f.read()
+            f.close()
+            return r
+        
+    elif path == "/m5/mission-start": # get pagina di start missione
+        with open("../Missioni/Missione5/missione.html") as f:
+            r = f.read()
+            f.close()
+            return r
+            
+            
 def check_post(path,clientchoice):
     pass
 
