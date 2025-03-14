a9a86ac0bbf8739753f07bc94bc59836a1cd60de
flekss07 <edopia007@gmail.com>
2025-03-14 16:43:44 +0100

    aggiunta base per pagina

--- Back-end/moduli/modulo_missione5/script.py
+++ Back-end/moduli/modulo_missione5/script.py
@@ -1,32 +1,26 @@
 from moduli import queryLib
-import os
 
 #queryLib.connetti()
 
 def check_get(path):
     if path == "/m5/styletest": # stest get fogli stile
-        with open("Missioni/Missione5/html_pages/style-template.css", "r") as f:
+        with open("../Missioni/Missione5/html_pages/style-template.css", "r") as f:
             r = f.read()
             f.close()
             return r
         
     elif path == "/m5/mission-start": # get pagina di start missione
-        with open("Missioni/Missione5/missione.html", "r") as f:
+        with open("../Missioni/Missione5/missione.html") as f:
             r = f.read()
             f.close()
             return r
+        
+    elif path == "/m5/castle-front": # get pagina di start missione
+        with open("../Missioni/Missione5/missione.html") as f:
+            r = f.read()
+            f.close()
+            return r 
     
-    elif path == "/m5/mission-start":
-        with open("Missioni/Missione5/assets/font.css", "r") as f:
-            r = f.read()
-            f.close()
-            return r
-    
-    elif path == "/m5/castle-front":
-        with open("Missioni/Missione5/assets/castle-front.jpg", "r") as f:
-            r = f.read()
-            f.close()
-            return r
             
             
 def check_post(path,clientchoice):
