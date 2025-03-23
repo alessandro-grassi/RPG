c24b366d0bb2038c2ec678152473b47dcfaec75a
flekss07 <edopia007@gmail.com>
2025-03-23 14:16:04 +0100

    creata funzione per fare post a server per index linee

--- Missioni/Missione5/missione.js
+++ Missioni/Missione5/missione.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded",()=>{ // caricare il testo
-    fetchData("get-dialogue",setLines);
+    fetchData("get-dialogue",setLines); // prende le linee di testo
+    fetchData("dialog-index",moveLines); // aggiorna l'index per le linee correnti e le prossime
 });
 
 let dialogLines; // variabile globale per lo store delle linee di dialogo da scorrere
@@ -21,10 +22,17 @@ function fetchData(request,callback)
     });
 }
 
-// funzione per impostare le linee di dialogo in base all'index preso dal server
+// funzione per impostare le linee di dialogo in base all'index preso dal server e far avanzare le immagini
 function updateLines()
 {
-    fetchData("dialog-index",moveLines);
+    fetchData("dialog-index",moveLines); // fa un fetch dell'index
+}
+
+// funzione che fa avanzare le linee di testo e aggiorna l'index lato server
+function moveLines(index)
+{
+    const data = {"current_index": index + 1}; // dati con index da inviare
+    sendToServer("update-index",data); // invia il nuovo index al server
 }
 
 // funzione che aggiusta il formato dele linee passate dal database e le inserisce in una variabile
@@ -33,16 +41,29 @@ function setLines(data)
     let lines = data.flatMap(obj => obj.text); // prende ogni linea di testo per gli oggetti estratti e la mappa all'oggetto
     console.log (lines); // log del risultato per check e debug
     dialogLines = lines; // assegna le linee di dialogo alla variabile globale
-    document.getElementById("dialog-box").textContent = lines[0];
-}
-
-// funzione che fa avanzare le linee di testo
-function moveLines(index)
-{
-    console.log (index);
-
 }
 
 // funzione che manda i dati al server
-function sendToServer()
-{}
+function sendToServer(request,data)
+{
+    fetch("http://localhost:8080/m5/" + request,{
+        method:"POST", // metodo richiesta
+        headers:{'Content-Type':'application/json'},
+        body: JSON.stringify(data) // dati da inviare in formato json
+    })
+    .then((response)=>{ // prende la risposta del server
+        if(!response.ok) // in caso di errore manda stato risposta a console
+        {
+            alert('operazione fallita, riprovare o ricaricare la pagina'); // in caso di fallimento post
+            throw new Error(`error posting content to server: ${response.status}`); 
+        }
+        return response.json(); // restituisce la risposta data dal server
+    })
+    .then((result)=>{
+        console.log(result); // log risposta per debug
+    })
+    .catch((err)=>{ // catch errori
+        console.error('error sending data: ',err); // fa un log a console dell'errore
+        alert('operazione fallita, riprovare o ricaricare la pagina.\n\ncodice di errore: '+ err); // manda un alert con il codice di errore
+    });
+}
