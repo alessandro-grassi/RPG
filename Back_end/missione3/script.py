c49c18e8cdb4b0952fe583d9401878bc0d98a537
SimoTheGamer <simone81mariuzzo@gmail.com>
2025-03-24 12:27:54 +0100

    aggiunta funzione per leggere i file

--- Back_end/missione3/script.py
+++ Back_end/missione3/script.py
@@ -1,11 +1,17 @@
 # import moduli.queryLib as queryLib
 #queryLib.connetti()
+def read_file(path):
+    f = open(path, "rb")
+    f_content = f.read()
+    f.close()
+    return f_content
+
 def check_get(path):
-    if path.endswith("get"):
-        print("get generica")
-        return "ciao".encode("utf-8")
+    if path.endswith("get_file"): # Invio di file al client
+        file_path = path.split("%")[1].split("-")[0]
+        return read_file("./Missioni/Missione3" + file_path)
 
 def check_post(path, client_choice):
     if path.endswith("post"):
         print("post generica")
-        return "ciao post".encode("utf-8")
+        return "ciao post"
