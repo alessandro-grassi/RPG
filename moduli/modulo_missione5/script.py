442389372d4a4a201c291353121f7a3d169e2a63
flekss07 <edopia007@gmail.com>
2025-03-26 14:51:10 +0100

    aggiornati missione.js e script.py per integrare scorrimento timeline dialoghi e immagini con salvataggio lato server da branch secondario

--- moduli/modulo_missione5/script.py
+++ moduli/modulo_missione5/script.py
@@ -1,5 +1,6 @@
 from moduli import queryLib
 import os
+import json
 #non funziona
 import moduli.modulo_missione5.combactSystem as combactSystem
 
@@ -32,19 +33,46 @@ def check_get(path:str):
             r = f.read()
             f.close()
             return r
+        
     elif path == PREFIX+"api-test":
         with open("Missioni/Missione5/api-test.html", "r") as f: # usare r per richieste sui file
             r = f.read()
             f.close()
             return r.encode("utf-8")
+        
     elif path.startswith( PREFIX_API+"get-life/"):
         name = path.split("/")[4]
         life = str (combactSystem.get_life(name))
         return ('{"result":"'+life+'"}').encode("utf-8")
+    
     elif path.startswith(PREFIX_API+"get-mana/"):
         name = path.split("/")[4]
         mana = str(combactSystem.get_mana(name))
         return ('{"result":"'+mana+'"}').encode("utf-8")
+    
+    # prende javascript missione
+    elif path == PREFIX + "script":
+        with open("Missioni/Missione5/missione.js") as f:
+            r = f.read()
+            f.close()
+            return r.encode("utf-8")
+    
+    # prende le linee di testo da far scorrere per i dialoghi
+    elif path == PREFIX + "get-dialogue":
+        with open("Missioni/Missione5/assets/dialogs.json", "r") as f:
+            r = f.read()
+            f.close()
+            return r.encode("utf-8") # encode per restituire i contenuti del file json come stringa da convertire in json dopo
+    
+    # prende index dialoghi per dialogo corrente
+    elif path == PREFIX + "dialog-index":
+        with open("Missioni/Missione5/assets/progress.json") as f:
+            r = f.read()
+            f.close()
+            return r.encode("utf-8")
+     
+    
+    
     
             
             
@@ -73,18 +101,40 @@ def check_post(path,clientchoice):
             value = clientchoice['value']
             combactSystem.use_mana(name, int(value))
             return '{"result":"Mana used successfully"}'.encode("utf-8")
+        
         elif path == PREFIX_API+"attack":
             attacker_name = clientchoice['attacker_name']
             attacked_name = clientchoice['attacked_name']
             attack_name = clientchoice['attack_name']
             combactSystem.attack(attacker_name, attacked_name, attack_name)
             return '{"result":"Attack executed successfully"}'.encode("utf-8")
+        
+        #aggiorna index dialoghi e immagini lore
+        elif path == PREFIX + "update-index":
+            print(clientchoice) # print per debug
+            update_index(clientchoice["current_index"])
+            return json.dumps({"status": "success"}).encode()  # Return JSON-encoded bytes
+        return json.dumps({"status": "error"}).encode()  # Ensure a valid JSON response
+
+        
     except KeyError as e:
         return f'{{"error":"Missing key {str(e)}"}}'.encode("utf-8")
     except ValueError as e:
         return f'{{"error":"Invalid value {str(e)}"}}'.encode("utf-8")
     except Exception as e:
         return f'{{"error":"An error occurred: {str(e)}"}}'.encode("utf-8")
+    
+
+# funzione usata per aggiornare l'index del dialogo a cui si è arrivati nella storia
+def update_index(index):
+    json_file = open("Missioni/Missione5/assets/progress.json", "r")  # legge il file json
+    parsed_data = json.loads(json_file.read()) # fa il parse in formato json della stringa
+    json_file.close()
+    parsed_data["current_index"] = index # imposta il nuovo index
+    converted_data = json.dumps(parsed_data, indent=4) # converte i dati in formato json
+    json_file = open("Missioni/Missione5/assets/progress.json", "w") # apre il file in lettura
+    json_file.write(converted_data) # scrive sul file json i dati
+    json_file.close() # chiude il file
 
 if __name__ == "__main__":
     print(
