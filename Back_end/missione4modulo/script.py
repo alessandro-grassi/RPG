48e9a36bc2f23590f6fcf0795853c6c0017040dc
ti-m0nt0 <montorfamarco06@gmail.com>
2025-03-26 10:51:51 +0100

    db json 26/03

--- Back_end/missione4modulo/script.py
+++ Back_end/missione4modulo/script.py
@@ -1,13 +1,19 @@
+import json #simulo il db
+
+#qualche json per simulare il db
+a = '{ "obiettivo": "scopri chi ha rapito Aldo Moro", "ricompensa": "titolo di Kung Fury", "tentativi": 5, "soluzione": "" }'
+
 def check_get(path):
-    if path.endswith("get_generica"):
-        return funz()
+    if path.endswith("dettagli"):
+        return getDettagli()
 
 def check_post(path, client_choice):
     if path.endswith("post_generica"):
         return funz222(client_choice)
 
-def funz():
-    print("ciao")
+def getDettagli():
+    
+
 
 def funz222(client_choice):
     nome = client_choice.get("nome", "")
