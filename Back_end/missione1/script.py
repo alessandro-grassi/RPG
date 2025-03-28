db523f013e64da6239476f1448604c957302ac02
tonykmiotek <antoniokmiotek0@gmail.com>
2025-03-28 22:27:49 +0100

    script

--- Back_end/missione1/script.py
+++ Back_end/missione1/script.py
@@ -1,21 +1,33 @@
+import json
+
 def check_get(path):
     if path.endswith("get_generica"):
         return 'ciao'.encode("utf-8")
-    elif path == "/missione1/index":
-        with open("Missioni/Missione1/index.html", "r") as file:  # Leggi il file index.html
-            content = file.read()
-            file.close()
-            return content.encode("utf-8")  # Restituisci il contenuto in formato bytes   
-    
-        
+    elif path.endswith("/index"):
+        try:
+            with open("./Missioni/Missione1/index.html", "r") as file:
+                content = file.read()
+            return content.encode("utf-8")
+        except FileNotFoundError:
+            return "File non trovato".encode("utf-8")
+    elif path.endswith("/script.js"):
+        try:
+            with open("./Missioni/Missione1/script.js", "r") as file:
+                content = file.read()
+            return content.encode("utf-8")
+        except FileNotFoundError:
+            return "File non trovato".encode("utf-8")
 
 
 
 
-def check_post(path,client_choice):
-    if path.endswit ("post_generica"):
-        return f2(client_choice)
+def check_post(path, client_choice):
+    if path.endswith("post_generica"):
+        return process_mission(client_choice)
+    else:
+        return "Percorso non valido".encode("utf-8")
 
-def f2(client_choice):
-    nome = client_choice.get("nome").encode("utf-8")
-    return nome
+
+
+
+
