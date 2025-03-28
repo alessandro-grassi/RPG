ac4aaf5f999031972355e29ec56c5584afa7d8b1
Arianna006 <194780372+Arianna006@users.noreply.github.com>
2025-03-28 17:41:38 +0100

    Sistemato il css completamente funzionante

--- Autenticazione/Login/login.js
+++ Autenticazione/Login/login.js
@@ -1,29 +1,16 @@
-/*var slideIndex = 1;
-showDivs(slideIndex);
+var k=0;
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+function changeVisibility(){
+  if(k == 0){
+    document.getElementById("registrazione").style.visibility = 'visible';
+    document.getElementById("accesso").style.visibility = "hidden";
+    k=1;
+  }else if(k == 1){
+    document.getElementById("registrazione").style.visibility = 'hidden';
+    document.getElementById("accesso").style.visibility = "visible";
+    k=0;
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
-} */
+}
 
 function registrazione(){
   const username= document.getElementById('n_username').value;
