eea30d9ce44c10a0b89414b67d5550acd8dbc853
Arianna006 <194780372+Arianna006users.noreply.github.com>
2025-03-28 16:07:58 +0100

    Sistemato il modulo python, da problemi con il salvataggio e la lettura presentando un errore json. Css ancora da sistemare. Completo con l'interrogazione SQL

--- Autenticazione/Login/login.js
+++ Autenticazione/Login/login.js
@@ -25,84 +25,83 @@ function showDivs(n) {
   dots[slideIndex-1].className += " w3-white";
 } */
 
-function registrazione(){
-  const username= document.getElementById('n_username').value;
-  const password= document.getElementById('n_password').value;
-  const email= document.getElementById('n_email').value;
-  const immagine= document.getElementById('n_image').value;
+  function registrazione(){
+    const username= document.getElementById('n_username').value;
+    const password= document.getElementById('n_password').value;
+    const email= document.getElementById('n_email').value;
+    const immagine= document.getElementById('n_image').value;
+    
+    if(username=="" || password == "" || email == ""){
+      alert("Non hai compilato tutti i campi")
+    }else{
+      const message={
+        user : username, 
+        pw : password,
+        mail : email
+      };
+      fetch('http://localhost:8080/login/registrazione', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(message)
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Errore nella risposta del server.');
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.error) {
+          alert("Errore di connessione, riprova più tardi!")
+        } else {
+          alert(data);
+        }
+      })
+      .catch(error => {
+        console.error("Errore:", error);
   
-  if(username=="" || password == "" || email == ""){
-    alert("Non hai compilato tutti i campi")
-  }else{
-    const message={
-      user : username, 
-      pw : password,
-      mail : email,
-      img: immagine
-    };
-    fetch('http://localhost:8080/login/registrazione', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(message)
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Errore nella risposta del server.');
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (data.error) {
-        alert("Errore di connessione, riprova più tardi!")
-      } else {
-        alert("Registrazione effettuata con successo");
-      }
-    })
-    .catch(error => {
-      console.error("Errore:", error);
-
-    });
+      });
+    }
+  
   }
-
-}
-
-function accedi(){
-  const username= document.getElementById('username').value;
-  const password= document.getElementById('password').value;
-  if(username == "" || password == ""){
-    alert("Non hai compilato tutti i campi")
-  }else{
-    const message={
-      user : username, 
-      pw : password
-    };
-    fetch('http://localhost:8080/login/accesso', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(message)
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Errore nella risposta del server.');
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (data.error) {
-        alert("Errore di connessione, riprova più tardi!")
-      } else {
-        alert("Accesso effettuato con successo");
-      }
-    })
-    .catch(error => {
-      console.error("Errore:", error);
-
-    });
+  
+  function accedi(){
+    const username= document.getElementById('username').value;
+    const password= document.getElementById('password').value;
+    if(username == "" || password == ""){
+      alert("Non hai compilato tutti i campi")
+    }else{
+      const message={
+        user : username, 
+        pw : password
+      };
+      fetch('http://localhost:8080/login/accesso', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(message)
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Errore nella risposta del server.');
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.error) {
+          alert("Errore di connessione, riprova più tardi!")
+        } else {
+          alert("Accesso effettuato con successo");
+        }
+      })
+      .catch(error => {
+        console.error("Errore:", error);
+  
+      });
+    }
   }
-}
 
 
