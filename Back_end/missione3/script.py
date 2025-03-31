4e8d6121a3e0b17648798221dbc6cb1482c7f5d8
SimoTheGamer <simone81mariuzzo@gmail.com>
2025-03-31 11:31:54 +0200

    ridefinito sistema file

--- Back_end/missione3/script.py
+++ Back_end/missione3/script.py
@@ -12,12 +12,12 @@ def read_text_file(path):
     return f_content.encode("utf-8")
 
 def check_get(path):
-    if path.endswith("get_text_file"): # Invio di file al client
-        file_path = path.split("%")[1].split("-")[0]
-        return read_text_file("./Missioni/Missione3" + file_path)
-    elif path.endswith("get_binary_file"): # Invio di file al client
-        file_path = path.split("%")[1].split("-")[0]
-        return read_binary_file("./Missioni/Missione3" + file_path)
+    if path.endswith("get_text"): # Invio di file al client
+        file_path = path.split("/")[2].split("-")[0]
+        return read_text_file("./Missioni/Missione3/" + file_path)
+    elif path.endswith("get_binary"): # Invio di file al client
+        file_path = path.split("/")[2].split("-")[0]
+        return read_binary_file("./Missioni/Missione3/" + file_path)
 
 def check_post(path, client_choice):
     if path.endswith("post"):
