cc2f0e764d9a5c710c34fdc15272b373d7737189
Thomas4004 <tq20032006@gmail.com>
2025-04-05 09:43:04 +0200

    Aggiunte GET e POST da file script js per ricevere dati e inviare risposta selezionata dall'utente. Aggiunto in script.py invio di file css e script js

--- Missioni/Missione5/script.js
+++ Missioni/Missione5/script.js
@@ -41,4 +41,54 @@ const esempio = {
     image: "https://spacejockeyreviews.com/wp-content/uploads/2013/08/Dr.-Malvegio-011.jpg"
 };
 
+async function getData(){ 
+    try{
+        const response = await fetch("nome server");
+        if (!response.ok){
+            throw new Error(`Errore HTTP: ${responde.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+        updateMission(data); //dati ricevuti nel seguente moto:
+        /*
+        description: "descrizione",
+        answers: ["risposta1", "risposta2"...],
+        image: "url immagine"
+        */
+    } catch(error) {
+        console.error("Errore durante la richiesta GET: ", error);
+        logResult({error: error.message});
+    }
+}
+
+async function inviaData(data){ //funzione per la POST della risposta selezionata dall'utente
+    try{
+        const response = await fetch("server name", {
+            method : "POST",
+            headers : { "Content-Type" : "application/json"},
+            body : JSON.stringify(data)
+        });
+        if(!response.ok){
+            throw new Error(`Errore HTTP: ${response.status}`);
+        }
+        const risposta = await response.json();
+        console.log(risposta);
+
+        if(risposta.result === 1){ //ricevo una risposta dal server con un JSON contenente il risultato, se è 1 allora è tutto corretto
+            alert("Risposta corretta!");
+            window.location.reload();
+            //DA IMPLEMENTARE IL PASSAGGIO ALLA MISSIONE SUCCESSIVA
+        }else if(risposta.result === 0){
+            alert("No hai sbagliato!");
+            window.location.reload();
+        }else{
+            alert("C'è stato un errore di comunicazione con il server!");
+            window.location.reload();
+        }
+    } catch(error){
+        console.error("Errore durante la richiesta POST: ", error);
+    }
+}
+
+
 updateMission(esempio);
