cd67e5a0ca9e9f9c49b6ab49db9767afdb8b3863
Arianna006 <194780372+Arianna006@users.noreply.github.com>
2025-04-12 09:13:08 +0200

    c

--- Back_end/personaggio/script.py
+++ Back_end/personaggio/script.py
@@ -16,7 +16,7 @@ def ottieni_classi():
     listaClassi = queryLib.execute(f'''SELECT classi.id FROM "classi" ''')
     queryLib.disconnetti()
     stringa = json.dumps(listaClassi)
-    return stringa
+    return stringa.encode("utf-8")
 
 
 
