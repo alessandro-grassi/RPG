cd268cd9dbe9a327494cd47bf75602eb162c99e3
AccountGithoob <ilmioangolochenonso@gmail.com>
2025-05-17 10:16:18 +0200

    test

---
+++ Missioni/test-strazzera/script.js
@@ -0,0 +1,9 @@
+function test(){
+    const container = document.getElementById("container");
+    container.textContent = 'Hello';
+}
+
+async function statistics() {
+    const container = document.getElementById("container");
+    const response = await fetch();
+}
