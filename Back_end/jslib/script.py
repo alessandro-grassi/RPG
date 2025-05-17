2a5a57d7ecf34f8934ece2c3a1174243283339d6
Bachk6 <simo.dv.06@gmail.com>
2025-05-17 09:11:48 +0200

    merge progress.json

--- Back_end/jslib/script.py
+++ Back_end/jslib/script.py
@@ -4,6 +4,11 @@ def check_get(path):
         r=f.read()
         f.close()
         return r.encode("utf-8")
+    if path.endswith("completa.js"){
+        f = open("db_lib.js","r")
+        r = f.read()
+        f.close()
+    }
     return "".encode("utf-8")
 def check_post(a,b):
     return "".encode("utf-8")
